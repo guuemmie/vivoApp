0eaf61cc8769453fec0242889c24259f8f1f1b67
tmoreti <thiago.moreti@gmail.com>
2016-04-03 16:15:46 -0300

    correcao qr code

--- assets/js/qrcode.js
+++ assets/js/qrcode.js
@@ -1,16 +1,16 @@
 function startScan() {
-
-    var url = cordova.plugins.barcodeScanner.scan(
+var url=''
+    cordova.plugins.barcodeScanner.scan(
         function (result) {
             /*var s = "Result: " + result.text + "<br/>" +
             "Format: " + result.format + "<br/>" +
             "Cancelled: " + result.cancelled;
             alert(s);*/
-            return result.text
+            url=result.text
         },
         function (error) {
             /*alert("Scanning failed: " + error);*/
-
+            url='erro';
         }
     );
     alert(url);
