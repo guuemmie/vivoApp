4e61a433ddf9a4236c19d557d8dc4ee69ca05d37
tmoreti <thiago.moreti@gmail.com>
2016-04-05 22:30:59 -0300

    php

--- paginas/palitagens.php
+++
@@ -1,9 +0,0 @@
-<?php
-	header('Access-Control-Allow-Origin: *');
-	header('Access-Control-Allow-Methods: GET, POST');
-
-	require_once('class/Palitagens.class.php');
-	$filtros=$_POST['filtros'];
-	$palitagens=new Palitagens($filtros);
-	echo $palitagens->info();
-?>
